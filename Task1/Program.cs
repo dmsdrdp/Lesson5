04a11a02ac49ec279d25a8b5c4e5b5d5481128b2
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-15 20:16:57 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                s += array[i];
+                k++;
+            }
+            Console.WriteLine(((double)s)/k);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = ((i==j)||(i + j == 2) || (i + j == n - 1) || (i+j == 6)) ? 1 : 0;
+                    Console.Write("{0}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
